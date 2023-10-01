3c09bedbf1c242e443e8035e145481cc1ae493f2
RB <ab@hyperscale.gr>
2023-10-01 18:30:31 +0300

    changes 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
-  def index
+ def index
+  student = current_student
+  if student_signed_in? && current_student.theses.present?
+   redirect_to student_path(id: student.theses.last.id)
+  elsif student_signed_in? && current_student.theses.empty?
+   redirect_to students_new_thesis_path
+  elsif professor_signed_in?
+   redirect_to professors_index_path
+  else
+    # for the users that aren't sign in
+  end
   end
 end
